4fa4c335e757a1921ae9a070b6f954055c58bb9f
Nitin <nvermadev@gmail.com>
2018-09-03 18:57:17 +0530

    modify mongo url for heroku

--- server/db.js
+++ server/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/users');
+mongoose.connect('mongodb://heroku_jjlb5976:t0ire3jrale3k3207vdkvt0b24@ds141932.mlab.com:41932/heroku_jjlb5976');
